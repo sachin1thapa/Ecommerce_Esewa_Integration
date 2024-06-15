e0ac080d31a5d31a8018f3f282db787a8a835e9f
Sachin1Thapa <yousachin328gmail.com>
2024-06-15 18:46:43 +0545

    finish contact me

---
+++ contact.js
@@ -0,0 +1,27 @@
+let form = document.querySelector('#form');
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  let username = document.querySelector('#username').value;
+  let UserEmail = document.querySelector('#email').value;
+  let message = document.querySelector('#message').value;
+  let subject = document.querySelector('#subject').value;
+  const body = 'Name: ' + username + '<br/> Email: ' + UserEmail + '<br/> Message: ' + message;
+
+  Email.send({
+    Host: 'smtp.elasticemail.com',
+    Username: 'thapasachin572@gmail.com',
+    Password: '8C87B3CA6F08E977D71F2E1E3DE56409B33C',
+    To: 'thapasachin572@gmail.com',
+    From: 'thapasachin572@gmail.com',
+    Subject: subject,
+    Body: body,
+  })
+    .then((message) => {
+      alert(message);
+      form.reset();
+    })
+    .catch((error) => {
+      alert('Error sending email: ' + error);
+    });
+});
